fd257a852d58f26aafc38b057dc0bfde08e0b8f1
fguq <holy.pollyanna@gmail.com>
2019-04-25 06:02:28 +0900

    finished fizz_buzz_03.rb (for now)

--- fizz_buzz_03.rb
+++ fizz_buzz_03.rb
@@ -1,10 +1,11 @@
-def digit_alignment(numbers)
-  sprintf("%03d", numbers)
-end
-
 (1..150).each do |num|
-  case num % 3
-  when 0
-    puts sprintf("%03d!", num)
+  if num % 3 == 0
+    num = num.to_s + "!" 
+    puts sprintf("%3s", num).gsub(/ /, "0")[0..2]
+  elsif num.to_s.include?("3")
+    num = num.to_s + "!" 
+    puts sprintf("%3s", num).gsub(/ /, "0")[0..2]
+  else
+    puts sprintf("%03d", num)
   end
 end
