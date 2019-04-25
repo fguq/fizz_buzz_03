a6a38f937e3bc8cf50f955a79d9a918206a9134d
fguq <holy.pollyanna@gmail.com>
2019-04-25 20:40:36 +0900

    finished fizz_buzz_03.rb

--- fizz_buzz_03.rb
+++ fizz_buzz_03.rb
@@ -1,10 +1,11 @@
+def number_output(number)
+  number = number.to_s + "!" 
+  puts sprintf("%3s", number).gsub(/ /, "0")[0..2]
+end
+
 (1..150).each do |num|
-  if num % 3 == 0
-    num = num.to_s + "!" 
-    puts sprintf("%3s", num).gsub(/ /, "0")[0..2]
-  elsif num.to_s.include?("3")
-    num = num.to_s + "!" 
-    puts sprintf("%3s", num).gsub(/ /, "0")[0..2]
+  if num % 3 == 0 || num.to_s.include?("3")
+    number_output(num)
   else
     puts sprintf("%03d", num)
   end
